5734c71d2d82601976f5c9ec6e4616451a5c1500
adesh12 <adeshchauhan22@gmail.com>
2020-06-18 10:46:29 +0530

    third commit

---
+++ WiproTalentNext/src/JavaFundamental_languagebasics/Quest_1.java
@@ -0,0 +1,11 @@
+package JavaFundamental_languagebasics;
+
+public class Quest_1 {
+
+	public static void main(String[] args) {
+		System.out.print(args[0] + " Technologies");
+		System.out.println(" "+args[1]);
+
+	}
+
+}
