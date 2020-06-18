9db18b2a07d1264186c936c4d6a2f679ba6bd304
adesh12 <adeshchauhan22@gmail.com>
2020-06-18 10:49:23 +0530

    fourth

---
+++ WiproTalentNext/src/JavaFundamental_languagebasics/Quest_3.java
@@ -0,0 +1,16 @@
+package JavaFundamental_languagebasics;
+
+public class Quest_3 {
+
+	public static void main(String[] args) {
+		int x=0;
+		int y=0;
+		int z=0;
+		x = Integer.parseInt(args[0]);
+		y = Integer.parseInt(args[1]);
+		z = x+y;
+		System.out.println("The sum of "+ x +" and "+ y +" is "+ z);
+
+	}
+
+}
